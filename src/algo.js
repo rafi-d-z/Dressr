2c2e260e4bf7958856c531606fb3943144bb482e
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-09-29 16:35:41 -0400

    added firebase api key

--- src/algo.js
+++ src/algo.js
@@ -1,45 +1,61 @@
 // once these items are created, i want to store them in a SQL db
 // the db will have the unique (primary key) of userID to link them
 class item{
-    constructor(name, color, size, type, season){
+    constructor(name, color, size, type, season="all", gender="neutral"){
         this.name = name;
         this.color = color;
         this.size = size;
         this.type = type; // type of clothing item
         this.season = season;
+        this.gender = gender;
+    }
+    copyFrom(otherItem) {
+        return new item(
+        otherItem.name,
+        otherItem.color,
+        otherItem.size,
+        otherItem.type,
+        otherItem.season,
+        otherItem.gender
+        );
     }
 }
 class wardrbe{
     // could implement a tree that stores clothes in relation to one another
     // this would make sorting through them and finding the optimal clothing item
     // more efficent and reduce time complexity
-    hats = new Set();
-    tops = new Set();
-    bottoms = new Set();
-    layers = new Set(); // jackets, sweaters, etc.
-    footwear = new Set();
+    hats = [];
+    tops = [];
+    bottoms = [];
+    layers = []; // jackets, sweaters, etc.
+    footwear = [];
+    accessories = [];
 
     add(item){
         switch(item.type){
             case item.type ="footwear":
-                this.footwear.add(item);
+                this.footwear.push(item);
                 break;
             case item.type ='layers':
-                this.layers.add(item);
+                this.layers.push(item);
                 break;
             case item.type ='bottoms':
-                this.bottoms.add(item);
+                this.bottoms.push(item);
                 break;
             case item.type ='tops':
-                this.tops.add(item);
+                this.tops.push(item);
                 break;
             case item.type =='hats':
-                this.hats.add(item);
+                this.hats.push(item);
+                break;
+            case item.type =='accessories':
+                this.accessories.push(item);
                 break;
             default:
-                console.log('Invalid Item Type');
+                console.log('Invalid Item Type:', item, '\nItem Type:', item.type);
                 break;
         }
+        return null;
     }
     // add functions for removing items, editing would be the same as removing and adding
     // a new item
@@ -52,54 +68,89 @@ class weather{
         this.season = season;
     }
 }
-function algo(wardrobe, weather) {
-  // Create a copy of the wardrobe to modify
-  const modifiedWardrobe = new wardrbe();
+function getRandomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length); //random index
+    let randElement = arr[randomIndex];
+    console.log(randElement.name);
+    return randElement.name, randElement.color, randElement.size, randElement.type, randElement.season, randElement.gender;
+}
+function algo(wardrobe, weather) { //maybe this should exist within the wardrbe class?
+    const modifiedWardrobe = new wardrbe();
+    const hats = [];
+    const tops = [];
+    const bottoms = [];
+    const layers = []; // jackets, sweaters, etc.
+    const footwear = [];
+    const accessories = [];
 
-  // Iterate over each clothing type and filter by season
-  for (const item of wardrobe.footwear) {
-    if (item.season !== weather.season) {
-      // Add to modified wardrobe only if it's not winter
-      modifiedWardrobe.add(item);
+    for (item in wardrobe.footwear) {
+        if (item.season == weather.season) {
+            footwear.push(item);
+        }
     }
-  }
+    let footwearItem = new item(getRandomElement(footwear));
 
-  for (const item of wardrobe.layers) {
-    if (item.season !== weather.season) {
-      // Add to modified wardrobe only if it's not summer
-      modifiedWardrobe.add(item);
+    for (item in wardrobe.layers) {
+        if (item.season == weather.season) {
+            layers.push(item);
+        }
     }
-  }
+    let layersItem = new item(getRandomElement(layers));
 
-  for (const item of wardrobe.tops) {
-    if (item.season !== weather.season) {
-      // Add to modified wardrobe only if it's not summer
-      modifiedWardrobe.add(item);
+    for (item in wardrobe.tops) {
+        if (item.season == weather.season) {
+            tops.push(item);
+        }
     }
-  }
+    let topsItem = new item(getRandomElement(tops));
 
-  for (const item of wardrobe.bottoms) {
-    if (item.season !== weather.season) {
-      // Add to modified wardrobe only if it's not summer
-      modifiedWardrobe.add(item);
+    for (item in wardrobe.bottoms) {
+        if (item.season == weather.season) {
+            bottoms.push(item);
+        }
     }
-  }
+    let bottomsItem = new item(getRandomElement(bottoms));
 
-  for (const item of wardrobe.hats) {
-    if (item.season !== weather.season) {
-      // Add to modified wardrobe only if it's not summer
-      modifiedWardrobe.add(item);
+    for (item in wardrobe.hats) {
+        if (item.season == weather.season) {
+            hats.push(item);
+        }
     }
-  }
+    let hatsItem = new item(getRandomElement(hats));
+
+    for (item in wardrobe.accessories) {
+        if (item.season == weather.season) {
+            accessories.push(item);
+        }
+    }
+    let accessoriesItem = new item(getRandomElement(accessories));
+    
+    modifiedWardrobe.add(footwearItem);
+    modifiedWardrobe.add(layersItem);
+    modifiedWardrobe.add(topsItem);
+    modifiedWardrobe.add(bottomsItem);
+    modifiedWardrobe.add(hatsItem);
+    modifiedWardrobe.add(accessoriesItem);
+
 
   // Return the modified wardrobe
   return modifiedWardrobe;
 }
 
 const wardrobe = new wardrbe();
-wardrobe.add(new item('Winter Boots', 'Black', 'L', 'footwear', 'winter'));
+wardrobe.add(new item('Winter Boots', 'Black', 'L', 'footwear', 'summer'));
 wardrobe.add(new item('Sneakers', 'White', 'M', 'footwear', 'summer'));
-wardrobe.add(new item('Winter Jacket', 'Blue', 'XL', 'layers', 'winter'));
+wardrobe.add(new item('Winter Jacket', 'Blue', 'XL', 'layers', 'summer'));
+wardrobe.add(new item('Leather Jacket', 'Brown', 'M', 'layers', 'summer'));
+wardrobe.add(new item('Slim Fit Jeans', 'Blue', '32', 'bottoms', 'summer'));
+wardrobe.add(new item('Cashmere Sweater', 'Gray', 'S', 'tops', 'summer'));
+wardrobe.add(new item('Ankle Boots', 'Taupe', '7', 'footwear', 'summer'));
+wardrobe.add(new item('Silk Blouse', 'Ivory', 'XS', 'tops', 'summer'));
+wardrobe.add(new item('Pleated Skirt', 'Burgundy', 'M', 'bottoms', 'summer'));
+wardrobe.add(new item('Trench Coat', 'Beige', 'L', 'layers', 'summer'));
+wardrobe.add(new item('High-Top Sneakers', 'White', '9', 'footwear', 'summer'));
+wardrobe.add(new item('Knit Beanie', 'Navy', 'One Size', 'accessories', 'summer'));
+wardrobe.add(new item('Floral Sundress', 'Pink', 'S', 'tops', 'summer'));
 
 const currentWeather = new weather(30, 10, 50, 'summer'); // Example weather conditions
 
--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,17 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "perfect-fit-51e0a.firebaseapp.com",
-    projectId: "perfect-fit-51e0a",
-    storageBucket: "perfect-fit-51e0a.appspot.com",
-    messagingSenderId: "1067698837842",
-    appId: "1:1067698837842:web:76f6ef27f5097800be428b",
-    measurementId: "G-2RFMYBGRHL"
+  apiKey: "AIzaSyAiGWev3lfggLh1ItaOOeWopeeX18iLW6E",
+  authDomain: "dressr-96ae1.firebaseapp.com",
+  projectId: "dressr-96ae1",
+  storageBucket: "dressr-96ae1.appspot.com",
+  messagingSenderId: "261836261265",
+  appId: "1:261836261265:web:23af5d79ba84e125ae7c30",
+  measurementId: "G-ZJZBGQZTS4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export default app;
